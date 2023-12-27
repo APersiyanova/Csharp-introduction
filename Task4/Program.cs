73021ef1042a55047e07dac9d8f74235753a65e9
APersiyanova <wrocla@mail.ru>
2023-12-27 22:14:59 +0300

    Cycles. Dumbell Task4.

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int f = 10;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (f > max)
+{
+    max = f;
+}
+Console.WriteLine(max);

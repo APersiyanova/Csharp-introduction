388c3f0ddcf3201892a6d1c9b5434d1a28ce182e
APersiyanova <wrocla@mail.ru>
2023-12-27 19:00:39 +0300

    Added a directory Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i1 = 5;
+int i2 = 6;
+int k = i1 + i2;
+Console.WriteLine(k);
+
+long l = 7;
+long g = l + i1;
+Console.WriteLine(g);
+
+double d = 8.5;
+double s = d + i1;
+Console.WriteLine(s);
+
+int product1 = i1 * i2;
+long product2 = l * i1;
+double product3 = d * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);

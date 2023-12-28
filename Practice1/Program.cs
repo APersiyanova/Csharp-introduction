7a5dfdfcbc21636c4241ba81b55fc8e266b763a5
APersiyanova <wrocla@mail.ru>
2023-12-28 17:59:02 +0300

    Practical tasks about C#

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = 9;
-int b = -3;
+// Программа на вход принимает 2 числа и проверяет, является ли первое квадратом второго
+//int a = 9;
+//int b = -3;
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+//int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+int b = int.Parse(Console.ReadLine());
 if (a == b * b)
 {
-    Console.WriteLine("да");
+    Console.WriteLine("да, первое число является квадратом второго");
 }
-else { Console.WriteLine("нет"); }
+else { Console.WriteLine("нет, первое число не является квадратом второго"); }

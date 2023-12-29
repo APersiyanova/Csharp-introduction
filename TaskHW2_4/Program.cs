eef1f1e239eba6821ec1a9b32c859343618dd4ab
APersiyanova <wrocla@mail.ru>
2023-12-30 00:47:46 +0300

    Submitted homework for review

--- TaskHW2_4/Program.cs
+++ TaskHW2_4/Program.cs
@@ -2,11 +2,15 @@
 // Программа на вход принимает натуральное число N, а на выходе показывает его цифры через запятую
 Console.WriteLine("Введите натуральное число");
 int N = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+
+int result;
 while (N>=10){
-    int result = N%10; 
-    Console.WriteLine(result);
-    N = N/10;
-    Console.WriteLine(N);
+    result = N%10; 
+    Console.Write(result + ",");
+    N /= 10;
 }
+result = N%10; 
+Console.WriteLine(result);
+
+
 
